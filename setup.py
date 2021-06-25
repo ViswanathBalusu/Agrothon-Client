514941be500bd6b312290ce2f07de9381a8bc31f
Chandra Kiran Viswanath Balusu <kiran.viswanath2017@vitstudent.ac.in>
2021-06-25 08:37:10 +0530

    Build Docker image in Github Actions itself

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ README = (CWD / "Readme.md").read_text()
 
 setup(
     name='AgroClient',
-    version="1.0.8",
+    version="1.1.0",
     packages=find_packages(),
     long_description=README,
     long_description_content_type="text/markdown",
