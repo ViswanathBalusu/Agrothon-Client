0dc632acfc96ca571ec8c1f1adfdbb299b058a00
Chandra Kiran Viswanath Balusu <kiran.viswanath2017@vitstudent.ac.in>
2021-06-25 08:45:13 +0530

    Bump Version

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ README = (CWD / "Readme.md").read_text()
 
 setup(
     name='AgroClient',
-    version="1.1.0",
+    version="1.1.1",
     packages=find_packages(),
     long_description=README,
     long_description_content_type="text/markdown",
